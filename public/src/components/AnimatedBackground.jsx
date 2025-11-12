908075926e98e20e425b29a1898ee9e4ecfcb5e8
VieiraGuilherme-dev <gv524003@gmail.com>
2025-11-12 15:39:50 -0300

    AnimatedBackground

---
+++ public/src/components/AnimatedBackground.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './AnimatedBackground.css';
+
+const AnimatedBackground = () => {
+  const stars = Array.from({ length: 50 });
+  return (
+    <div className="stars">
+      {stars.map((_, i) => (
+        <div key={i} className="star"></div>
+      ))}
+    </div>
+  );
+};
+
+export default AnimatedBackground;
